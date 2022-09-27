68544bd60048d2462334717c977b6c87c69e447e
Indian Ocean Roleplay <indianoceanroleplay@gmail.com>
2022-09-27 14:09:27 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/cfc-documentation/',
+  title: 'Coins For College Documentation',
+  tagline: 'Learn how to use our dashboards and find answers to most common questions asked by our users',
+  url: 'https://docs.coinsforcollege.org/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'coinsforcollege', // Usually your GitHub org/user name.
+  projectName: 'cfc-documentation', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
